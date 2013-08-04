c2877df76bdd2ee74ce2c909f3eeb8fbfb2e908a
Mekire <3ror@live.com>
2013-08-04 21:39:46 +0900

    Added topdown_scrolling example.

---
+++ topdown_scrolling/scrolling.py
@@ -0,0 +1,171 @@
+import os
+import sys
+import pygame as pg
+
+
+CAPTION = "Scrolling Background"
+
+
+DIRECT_DICT = {pg.K_UP   : ( 0,-1),
+               pg.K_DOWN : ( 0, 1),
+               pg.K_RIGHT: ( 1, 0),
+               pg.K_LEFT : (-1, 0)}
+
+
+class Player(object):
+    """Our user controllable character."""
+    def __init__(self,location,speed):
+        """The location is an (x,y) coordinate; speed is in pixels per frame.
+        The location of the player is with respect to the map he is in, not the
+        display screen."""
+        self.speed = speed
+        self.image = PLAY_IMG
+        self.mask = pg.mask.from_surface(self.image)
+        self.rect = self.image.get_rect(center=location)
+
+    def update(self,level_mask,keys):
+        """Check pressed keys to find initial movement vector.  Then call
+        collision detection methods and adjust the vector appropriately."""
+        move = self.check_keys(keys)
+        self.check_collisions(move,level_mask)
+
+    def draw(self,surface):
+        """Basic draw function."""
+        surface.blit(self.image,self.rect)
+
+    def check_keys(self,keys):
+        """Find the players movement vector from key presses."""
+        move = [0,0]
+        for key in DIRECT_DICT:
+            if keys[key]:
+                for i in (0,1):
+                    move[i] += DIRECT_DICT[key][i]*self.speed
+        return move
+
+    def check_collisions(self,move,level_mask):
+        """Call collision_detail for the x and y components of our movement
+        vector."""
+        x_change = self.collision_detail(move,level_mask,0)
+        self.rect.move_ip((x_change,0))
+        y_change = self.collision_detail(move,level_mask,1)
+        self.rect.move_ip((0,y_change))
+
+    def collision_detail(self,move,level_mask,index):
+        """Check for collision and if found decrement vector by single pixels
+        until clear."""
+        test_offset = list(self.rect.topleft)
+        test_offset[index] += move[index]
+        while level_mask.overlap_area(self.mask,test_offset):
+            move[index] += (1 if move[index]<0 else -1)
+            test_offset = list(self.rect.topleft)
+            test_offset[index] += move[index]
+        return move[index]
+
+
+class Level(object):
+    """A class for our map.  Maps in this implementation are one image; not
+    tile based.  This makes collision detection simpler but can have performance
+    implications."""
+    def __init__(self,map_image,viewport,player):
+        """Requires an image from which to make a mask, and a player
+        instance."""
+        self.image = map_image
+        self.mask = pg.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+        self.player = player
+        self.player.rect.center = self.rect.center  #Start at center of map.
+        self.viewport = viewport
+
+    def update(self,keys):
+        """Updates the player and then adjust the viewport with respect to the
+        player's new position."""
+        self.player.update(self.mask,keys)
+        self.update_viewport()
+
+    def update_viewport(self):
+        """The viewport will stay centered on the player unless the player
+        approaches the edge of the map."""
+        width,height = self.viewport.size
+        if self.player.rect.centerx <= width//2:
+            self.viewport.x = 0
+        elif self.player.rect.centerx >= self.rect.width-width//2:
+            self.viewport.x = self.rect.width-width
+        else:
+            self.viewport.centerx = self.player.rect.centerx
+        if self.player.rect.centery <= height//2:
+            self.viewport.y = 0
+        elif self.player.rect.centery >= self.rect.height-height//2:
+            self.viewport.y = self.rect.height-height
+        else:
+            self.viewport.centery = self.player.rect.centery
+
+    def draw(self,surface):
+        """Blit actors onto a copy of the map image; then blit the viewport
+        portion of that map onto the display surface."""
+        new_image = self.image.copy()
+        self.player.draw(new_image)
+        surface.fill((50,255,50))
+        surface.blit(new_image,(0,0),self.viewport)
+
+
+class Control(object):
+    """We meet again."""
+    def __init__(self):
+        """Initialize things; create a Player; create a Level."""
+        self.screen = pg.display.get_surface()
+        self.screen_rect = self.screen.get_rect()
+        self.clock = pg.time.Clock()
+        self.fps = 60.0
+        self.keys = pg.key.get_pressed()
+        self.done = False
+        self.player = Player((0,0),7)
+        self.level = Level(POND_IMG,self.screen_rect,self.player)
+
+    def event_loop(self):
+        """A quiet day in the neighborhood here."""
+        for event in pg.event.get():
+            self.keys = pg.key.get_pressed()
+            if event.type == pg.QUIT or self.keys[pg.K_ESCAPE]:
+                self.done = True
+
+    def update(self):
+        """Update the level.  In this implementation player updateing is taken
+        care of by the level update function."""
+        self.screen.fill((0))
+        self.level.update(self.keys)
+        self.level.draw(self.screen)
+        caption = "{} - FPS: {:.2f}".format(CAPTION,self.clock.get_fps())
+        pg.display.set_caption(caption)
+
+    def main_loop(self):
+        """...and we run in circles."""
+        while not self.done:
+            self.event_loop()
+            self.update()
+            pg.display.update()
+            self.clock.tick(self.fps)
+
+
+def image_from_url(url):
+    """Load an image from an url.  The return can be loaded by
+    pygame.image.load.  Python 2 and 3 compatible."""
+    try:
+        from urllib2 import urlopen
+        from cStringIO import StringIO as inout
+    except ImportError:
+        from urllib.request import urlopen
+        from io import BytesIO as inout
+    myurl = urlopen(url)
+    return inout(myurl.read())
+
+
+if __name__ == "__main__":
+    os.environ['SDL_VIDEO_CENTERED'] = '1'
+    pg.init()
+    pg.display.set_mode((700,500))
+    PLAY_IMG  = pg.image.load("smallface.png").convert_alpha()
+    POND_IMG  = pg.image.load("pond.png").convert_alpha()
+    run_it = Control()
+    run_it.main_loop()
+    pg.quit()
+    sys.exit()
