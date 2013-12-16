1eb7e49407e5415db0f43188d93afd68ca73fda0
Mekire <3ror@live.com>
2013-12-16 09:19:09 +0900

    Isolated draw logic in punch.py

--- punch_a_hole/punch.py
+++ punch_a_hole/punch.py
@@ -1,7 +1,7 @@
 """
-Demonstrates two different methods of punching a hole in a surface.
+This sample demonstrates two different methods of punching a hole in a surface.
 I then extend one of the methods in gradient_hole to show more complex effects.
-The hole will follow the mouse cursor.  The three different methods are called
+The hole will follow the mouse cursor. The three different methods are called
 in the update function; uncomment the one you would like to test, and comment
 out the other two.  Various comments also indicate parameters that you are
 encouraged to experiment with.
@@ -12,6 +12,11 @@ encouraged to experiment with.
 import os
 import sys
 import pygame as pg
+
+CAPTION = "Punching a Hole"
+SCREEN_SIZE = (1000,650)
+COLOR_KEY = (255,0,255)
+ELLIPSE_SIZE = (400,400)
 
 
 class Control(object):
@@ -25,37 +30,40 @@ class Control(object):
         self.done = False
         self.keys = pg.key.get_pressed()
         self.background = FRACTAL
-        self.ellipse_rect = pg.Rect((0,0,400,400))
+        self.ellipse_rect = pg.Rect((0,0),ELLIPSE_SIZE)
+        self.hole = None
 
     def event_loop(self):
-        """We don't have much to do here in this one, but it is still essential
-        that this runs every frame."""
+        """We don't have much to do here, but it is still essential that this
+        runs every frame."""
         for event in pg.event.get():
             self.keys = pg.key.get_pressed()
             if event.type == pg.QUIT or self.keys[pg.K_ESCAPE]:
                 self.done = True
 
     def make_hole(self):
-        """This method uses convert() surfaces with a colorkey."""
+        """This method uses a convert() surface with a colorkey. For our
+        purposes here, using a convert_alpha() surface actually performs far
+        better than this one (see make_hole_alpha() below)."""
         hole = pg.Surface(self.screen_rect.size).convert()
-        hole.set_colorkey((255,0,255))
+        hole.set_colorkey(COLOR_KEY)
         hole.fill((0,0,0)) #Experiment with changing this color.
-        pg.draw.ellipse(hole,(255,0,255), self.ellipse_rect)
+        pg.draw.ellipse(hole,COLOR_KEY,self.ellipse_rect)
 ##        hole.set_alpha(200) #Experiment with uncommenting/changing this value.
-        self.screen.blit(hole,(0,0))
+        return hole
 
     def make_hole_alpha(self):
         """This method uses convert_alpha() and has no need for color keys."""
         hole = pg.Surface(self.screen_rect.size).convert_alpha()
         hole.fill((255,255,255,200)) #Experiment with changing this color
         pg.draw.ellipse(hole,(0,0,0,0), self.ellipse_rect)
-        self.screen.blit(hole,(0,0))
+        return hole
 
     def gradient_hole(self):
-        """This method is a modification on the make_hole_alpha method.  It will
-        draw a series of ellipses that decrease in size according to step, and
-        decrease in alpha according to alpha_step.  This allows us to create
-        some fairly simple gradient style effects."""
+        """This method is a modification on the make_hole_alpha() method.
+        It will draw a series of ellipses that decrease in size according to
+        step, and decrease in alpha according to alpha_step. This allows us to
+        create some fairly simple gradient style effects."""
         hole = pg.Surface(self.screen_rect.size).convert_alpha()
         color = pg.Color(50,0,0,255) #Experiment with changing this color
         hole.fill(color)
@@ -72,29 +80,40 @@ class Control(object):
             #but in the case of a step of -1,-1 it fails.
             shrink_rect.inflate_ip(step)
             shrink_rect.center = self.ellipse_rect.center
-        self.screen.blit(hole,(0,0))
+        return hole
 
     def update(self):
         """Uncomment the version you want to test."""
         self.ellipse_rect.center = pg.mouse.get_pos()
+        self.hole = self.make_hole()
+##        self.hole = self.make_hole_alpha()
+##        self.hole = self.gradient_hole()
+
+    def draw(self):
+        """It is always a good idea to isolate drawing logic."""
         self.screen.blit(self.background,(0,0))
-        self.make_hole()
-##        self.make_hole_alpha()
-##        self.gradient_hole()
+        self.screen.blit(self.hole,(0,0))
+
+    def display_fps(self):
+        """Show the program's FPS in the window handle."""
+        caption = "{} - FPS: {:.2f}".format(CAPTION,self.clock.get_fps())
+        pg.display.set_caption(caption)
 
     def main_loop(self):
         """Spin me right round."""
         while not self.done:
             self.event_loop()
             self.update()
+            self.draw()
             pg.display.update()
             self.clock.tick(self.fps)
+            self.display_fps()
 
 
 if __name__ == "__main__":
     os.environ['SDL_VIDEO_CENTERED'] = '1'
     pg.init()
-    pg.display.set_mode((1000,650))
+    pg.display.set_mode(SCREEN_SIZE)
     FRACTAL = pg.image.load("frac.jpg").convert()
     run_it = Control()
     run_it.main_loop()
