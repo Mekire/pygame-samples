46807eb47e151449ac911075600d58ee132f0544
Mekire <3ror@live.com>
2015-08-19 15:14:02 -0700

    Cleaned color_change.py

--- color_change.py
+++ color_change.py
@@ -1,5 +1,9 @@
+#! /usr/bin/env python
+
 """
-Basics introduction. Change the screen color with a mouse click.
+This example serves as a basics introduction.
+Change the screen color with a mouse click.
+
 This program is intentionally over-commented to serve as an introduction for
 those with no knowledge of pygame. More advanced examples will not adhere to
 this convention.
@@ -7,36 +11,50 @@ this convention.
 -Written by Sean J. McKiernan 'Mekire'
 """
 
-import os  #Used for os.environ.
-import sys  #Used for sys.exit.
-import random  #Used for random.randint.
-import pygame as pg  #I abbreviate pygame as pg for brevity.
+import os # Used for os.environ.
+import sys # Used for sys.exit.
+import random # Used for random.randint.
+
+import pygame as pg # I abbreviate pygame as pg for brevity.
 
 
-class Control(object):
-    """A control class to manage our event and game loops."""
+CAPTION = "Click to Change My Color"
+SCREEN_SIZE = (500, 500)
+
+
+class App(object):
+    """
+    This is the main class for our application.
+    It manages our event and game loops.
+    """
     def __init__(self):
-        """Get pygame ready and define our start color as black."""
-        os.environ['SDL_VIDEO_CENTERED'] = '1'  #Center the screen.
-        pg.init()  #Initialize Pygame.
-        pg.display.set_caption("Click to Change My Color.")
-        self.screen = pg.display.set_mode((500, 500))  #Prepare the screen.
-        self.clock = pg.time.Clock()  #Create a clock to restrict framerate.
-        self.fps = 60.0  #Define your max framerate.
-        self.done = False  #A flag to tell our game when to quit.
-        self.keys = pg.key.get_pressed()  #All the keys currently held.
-        self.color = 0  #The screen will start as black.
+        """
+        Get a reference to the screen (created in main); define necessary
+        attributes; and set our starting color to black.
+        """
+        self.screen = pg.display.get_surface() # Get reference to the display.
+        self.clock = pg.time.Clock() # Create a clock to restrict framerate.
+        self.fps = 60 # Define your max framerate.
+        self.done = False # A flag to tell our game when to quit.
+        self.keys = pg.key.get_pressed() # All the keys currently held.
+        self.color = pg.Color("black") # The screen will start as black.
 
     def event_loop(self):
-        """Our event loop; called once every frame."""
-        for event in pg.event.get():  #Check the events on the event queue.
-            self.keys = pg.key.get_pressed()
+        """
+        Our event loop; called once every frame.  Only things relevant to
+        processing specific events should be here.  It should not
+        contain any drawing/rendering code.
+        """
+        for event in pg.event.get():  # Check each event in the event queue.
             if event.type == pg.QUIT or self.keys[pg.K_ESCAPE]:
-                #If the user presses escape or closes the window we're done.
+                # If the user presses escape or closes the window we're done.
                 self.done = True
             elif event.type == pg.MOUSEBUTTONDOWN:
-                #If the user clicks the screen, change the color.
+                # If the user clicks the screen, change the color.
                 self.color = [random.randint(0, 255) for _ in range(3)]
+            elif event.type in (pg.KEYUP, pg.KEYDOWN):
+                # Update keys when a key is pressed or released.
+                self.keys = pg.key.get_pressed() 
 
     def main_loop(self):
         """
@@ -44,14 +62,24 @@ class Control(object):
         restricts the framerate; and loops.
         """
         while not self.done:
-            self.event_loop()  #Run the event loop every frame.
-            self.screen.fill(self.color)  #Fill the screen with the new color.
-            pg.display.update()  #Make updates to screen every frame.
-            self.clock.tick(self.fps)  #Restrict framerate of program.
+            self.event_loop() # Run the event loop every frame.
+            self.screen.fill(self.color) # Fill the screen with the new color.
+            pg.display.update() # Make updates to screen every frame.
+            self.clock.tick(self.fps) # Restrict framerate of program.
+
+
+def main():
+    """
+    Prepare our environment, create a display, and start the program.
+    """
+    os.environ['SDL_VIDEO_CENTERED'] = '1' # Center the window (optional).
+    pg.init() # Initialize Pygame.
+    pg.display.set_caption(CAPTION) # Set the caption for the window.
+    pg.display.set_mode(SCREEN_SIZE) # Prepare the screen.
+    App().main_loop()
+    pg.quit()
+    sys.exit()
 
 
 if __name__ == "__main__":
-    run_it = Control()
-    run_it.main_loop()
-    pg.quit()
-    sys.exit()
+    main()
