3362f8b273c1cc5f59cc2e278dadd496f2a90099
Mekire <3ror@live.com>
2015-08-20 21:12:15 -0700

    Clean eight_dir_movement_adjusted.py

--- eight_dir_movement_adjusted.py
+++ eight_dir_movement_adjusted.py
@@ -1,10 +1,12 @@
+#! /usr/bin/env python
+
 """
 Script is identical to eight_dir_move with an adjustment so that the player
 moves at the correct speed when travelling at an angle. The script is also
-changed to use a time step to update the player. This means that speed is now
-in pixels per second, rather than pixels per frame. The advantage of this is
-that the game will always update at the same speed, regardless of any frame
-loss.
+changed to use a variable time step to update the player.
+This means that speed is now in pixels per second, rather than pixels per
+frame. The advantage of this is that the game will always update at the same
+speed, regardless of any frame loss.
 
 -Written by Sean J. McKiernan 'Mekire'
 """
@@ -12,14 +14,13 @@ loss.
 import os
 import sys
 import math
+
 import pygame as pg
 
 
+CAPTION = "Move me with the Arrow Keys"
 SCREEN_SIZE = (500, 500)
 
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-BLACK = (0, 0, 0)
 TRANSPARENT = (0, 0, 0, 0)
 
 DIRECT_DICT = {pg.K_LEFT  : (-1, 0),
@@ -27,33 +28,42 @@ DIRECT_DICT = {pg.K_LEFT  : (-1, 0),
                pg.K_UP    : ( 0,-1),
                pg.K_DOWN  : ( 0, 1)}
 
-#X and Y Component magnitude when moving at 45 degree angles
+# X and Y Component magnitude when moving at 45 degree angles
 ANGLE_UNIT_SPEED = math.sqrt(2)/2
 
 
 class Player(object):
-    """This class will represent our user controlled character."""
-    def __init__(self, speed, *rect):
+    """
+    This class will represent our user controlled character.
+    """
+    SIZE = (100, 100)
+    
+    def __init__(self, pos, speed):
         """
-        Arguments are the player's speed (in pixels/second) and the player's
-        rect (all rect style arguments accepted).
+        The pos argument is a tuple for the center of the player (x,y);
+        speed is given in pixels/second.
         """
-        self.rect = pg.Rect(rect)
-        self.move = list(self.rect.center)
+        self.rect = pg.Rect((0,0), Player.SIZE)
+        self.rect.center = pos
+        self.true_pos = list(self.rect.center)
         self.speed = speed
         self.image = self.make_image()
 
     def make_image(self):
-        """Creates our hero (a red circle/ellipse with a black outline)."""
+        """
+        Creates our hero (a red circle/ellipse with a black outline).
+        """
         image = pg.Surface(self.rect.size).convert_alpha()
         image.fill(TRANSPARENT)
         image_rect = image.get_rect()
-        pg.draw.ellipse(image, BLACK, image_rect)
-        pg.draw.ellipse(image, RED, image_rect.inflate(-12, -12))
+        pg.draw.ellipse(image, pg.Color("black"), image_rect)
+        pg.draw.ellipse(image, pg.Color("red"), image_rect.inflate(-12, -12))
         return image
 
     def update(self, screen_rect, keys, dt):
-        """Updates our player appropriately every frame."""
+        """
+        Updates our player appropriately every frame.
+        """
         vector = [0, 0]
         for key in DIRECT_DICT:
             if keys[key]:
@@ -61,59 +71,80 @@ class Player(object):
                 vector[1] += DIRECT_DICT[key][1]
         factor = (ANGLE_UNIT_SPEED if all(vector) else 1)
         frame_speed = self.speed*factor*dt
-        self.move[0] += vector[0]*frame_speed
-        self.move[1] += vector[1]*frame_speed
-        self.rect.center = self.move
+        self.true_pos[0] += vector[0]*frame_speed
+        self.true_pos[1] += vector[1]*frame_speed
+        self.rect.center = self.true_pos
         if not screen_rect.contains(self.rect):
             self.rect.clamp_ip(screen_rect)
-            self.move = list(self.rect.center)
+            self.true_pos = list(self.rect.center)
 
     def draw(self, surface):
-        """Draws the player to the target surface."""
+        """
+        Draws the player to the target surface.
+        """
         surface.blit(self.image, self.rect)
 
 
-class Control(object):
-    """Keep things under control."""
+class App(object):
+    """
+    A class to manage our event, game loop, and overall program flow.
+    """
     def __init__(self):
-        """Initializing in the init; how novel."""
-        os.environ['SDL_VIDEO_CENTERED'] = '1'
-        pg.init()
-        pg.display.set_caption("Move me with the Arrow Keys.")
-        self.screen = pg.display.set_mode(SCREEN_SIZE)
+        """
+        Get a reference to the display surface; set up required attributes;
+        and create a Player instance.
+        """
+        self.screen = pg.display.get_surface()
         self.screen_rect = self.screen.get_rect()
         self.clock = pg.time.Clock()
-        self.fps = 60.0
+        self.fps = 60
         self.done = False
         self.keys = pg.key.get_pressed()
-        self.player = self.make_player()
-
-    def make_player(self):
-        """Create a player and set player.move and player.rect.center equal."""
-        player = Player(190, (0, 0, 100, 100))
-        player.move = list(self.screen_rect.center)
-        return player
+        self.player = Player(self.screen_rect.center, 190)
 
     def event_loop(self):
-        """One event loop. Never cut your game off from the event loop."""
+        """
+        One event loop. Never cut your game off from the event loop.
+        """
         for event in pg.event.get():
-            self.keys = pg.key.get_pressed()
             if event.type == pg.QUIT or self.keys[pg.K_ESCAPE]:
                 self.done = True
+            elif event.type in (pg.KEYUP, pg.KEYDOWN):
+                self.keys = pg.key.get_pressed() 
 
+    def render(self):
+        """
+        Perform all necessary drawing and update the screen.
+        """
+        self.screen.fill(pg.Color("white"))
+        self.player.draw(self.screen)
+        pg.display.update()
+        
     def main_loop(self):
-        """One game loop. Simple and clean."""
+        """
+        One game loop. Simple and clean.
+        """
+        self.clock.tick(self.fps)/1000.0
+        dt = 0.0
         while not self.done:
-            time_delta = self.clock.tick(self.fps)/1000.0
             self.event_loop()
-            self.player.update(self.screen_rect, self.keys, time_delta)
-            self.screen.fill(WHITE)
-            self.player.draw(self.screen)
-            pg.display.update()
+            self.player.update(self.screen_rect, self.keys, dt)
+            self.render()
+            dt = self.clock.tick(self.fps)/1000.0
 
 
-if __name__ == "__main__":
-    run_it = Control()
-    run_it.main_loop()
+def main():
+    """
+    Prepare our environment, create a display, and start the program.
+    """
+    os.environ['SDL_VIDEO_CENTERED'] = '1'
+    pg.init()
+    pg.display.set_caption(CAPTION)
+    pg.display.set_mode(SCREEN_SIZE)
+    App().main_loop()
     pg.quit()
     sys.exit()
+    
+
+if __name__ == "__main__":
+    main()
